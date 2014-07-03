15e705e4a5e04fa0bc85947af56172e6c8904214
Aemon Cannon <aemoncannon@gmail.com>
2014-07-03 14:18:05 -0400

    yet again

--- .travis.sh
+++ .travis.sh
@@ -8,6 +8,9 @@ mkdir ~/.emacs.d/ensime-servers
 mkdir ~/.emacs.d/ensime-servers/${ENSIME_TEST_SERVER_VERSION}
 touch ensime_test.log
 $EMACS --version
+echo "Updating server:"
 $EMACS --batch --no-init-file --load .emacs_test.el --eval "(ensime-update-server \"${ENSIME_TEST_SERVER_VERSION}\")"
+sleep 2
+echo "Starting tests:"
 $EMACS -d :99 --no-init-file --load .emacs_test.el --eval "(ensime-run-all-tests)" &
 tail -f ensime_test.log
