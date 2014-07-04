d1f6850a692d72ae28d51469c7535f3b9887c198
Aemon Cannon <aemoncannon@gmail.com>
2014-07-04 14:36:53 -0400

    Merged

--- .travis.sh
+++ .travis.sh
@@ -3,14 +3,7 @@
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 sleep 3
-mkdir ~/.emacs.d
-mkdir ~/.emacs.d/ensime-servers
-mkdir ~/.emacs.d/ensime-servers/${ENSIME_TEST_SERVER_VERSION}
 touch ensime_test.log
 $EMACS --version
-echo "Updating server:"
-$EMACS --batch --no-init-file --load .emacs_test.el --eval "(ensime-get-or-download-server \"${ENSIME_TEST_SERVER_VERSION}\")"
-sleep 2
-echo "Starting tests:"
 $EMACS -d :99 --no-init-file --load .emacs_test.el --eval "(ensime-run-all-tests)" &
 tail -f ensime_test.log
