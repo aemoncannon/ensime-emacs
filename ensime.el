2b8e0c3e03b70b56b68a2e64a1a02656b3457f6b
Aemon Cannon <aemoncannon@gmail.com>
2014-07-03 22:48:49 -0400

    yet again

--- ensime.el
+++ ensime.el
@@ -1409,7 +1409,7 @@ The functions are called with the process as their argument.")
   (with-current-buffer (process-buffer process)
     (goto-char (point-max))
     (insert string))
-  (message string)
+  (write-region string nil "ensime_test.log" 'append)
   (ensime-process-available-input process))
 
 (defun ensime-process-available-input (process)
