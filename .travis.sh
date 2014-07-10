3d02304107186ad48488d09807aa1851337e5e4c
Aemon Cannon <aemoncannon@gmail.com>
2014-07-09 23:32:44 -0400

    Trying to log server output.

--- .travis.sh
+++ .travis.sh
@@ -6,4 +6,4 @@ sleep 3
 touch ensime_test.log
 $EMACS --version
 $EMACS -d :99 --no-init-file --load .emacs_test.el --eval "(ensime-run-all-tests)" &
-tail -f /tmp/ensime_test.log
+tail -f /home/travis/ensime_test.log
